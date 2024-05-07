29265aa01a8dd56a75465f860f99962ebe9933a8
thunchakorn <thunchakorn.s@avareum.finance>
2024-05-07 13:20:05 +0700

    add FilmDetailView

--- films/views.py
+++ films/views.py
@@ -1,8 +1,11 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from films.models import Film
 
 class FilmListView(ListView):
+    model = Film
+
+class FilmDetailView(DetailView):
     model = Film
 
 
