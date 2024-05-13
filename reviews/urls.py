cb3644afef0ccd4c4b13417815dbd187a9996f55
thunchakorn <s.thunchakorn@gmail.com>
2024-05-13 20:23:34 +0700

    add review detail page

--- reviews/urls.py
+++ reviews/urls.py
@@ -6,7 +6,7 @@ app_name = "reviews"
 
 urlpatterns = [
     path("", views.ReviewListView.as_view(), name="feed"),
-    path("reviews/create/", views.ReviewCreateView.as_view(), name="create"),
-    path("reviews/<int:pk>/", views.ReviewDetailView.as_view(), name="detail"),
-    path("reviews/<int:pk>/like/", views.LikeReview.as_view(), name="like-toggle"),
+    path("create/", views.ReviewCreateView.as_view(), name="create"),
+    path("<int:pk>/", views.ReviewDetailView.as_view(), name="detail"),
+    path("<int:pk>/like/", views.LikeReview.as_view(), name="like-toggle"),
 ]
