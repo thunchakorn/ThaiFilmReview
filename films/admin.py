8aa1006ec167ace6bd09f5762fb7ed5a4d8a0499
thunchakorn <s.thunchakorn@gmail.com>
2024-05-15 16:17:07 +0700

    add link to FilmDetailView template

--- films/admin.py
+++ films/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Genre, Person, Film, Role
+from .models import Genre, Person, Film, Role, Link
 
 admin.site.register(Genre)
 admin.site.register(Person)
 admin.site.register(Film)
 admin.site.register(Role)
+admin.site.register(Link)
