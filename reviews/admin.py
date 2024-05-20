e70f2c441acf1d5a03cb7e91fa37e8cfb858d418
thunchakorn <s.thunchakorn@gmail.com>
2024-05-20 13:27:58 +0700

    add ReviewAdmin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 from reviews.models import Review, Comment, Like
 
-admin.site.register(Review)
+
+class ReviewAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["profile", "film"]}),
+        (
+            "Rating",
+            {
+                "fields": [
+                    "rating",
+                    "screenplay_rating",
+                    "acting_rating",
+                    "production_rating",
+                    "cinematography_rating",
+                ]
+            },
+        ),
+        ("Review", {"fields": ["is_spoiler", "short_review", "full_review"]}),
+    ]
+    list_display = ["__str__", "is_spoiler"]
+
+
+admin.site.register(Review, ReviewAdmin)
 admin.site.register(Comment)
 admin.site.register(Like)
