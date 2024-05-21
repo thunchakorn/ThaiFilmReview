f20e88e40ed8a12d6b5c6f51dcd8648b8ed14f05
thunchakorn <s.thunchakorn@gmail.com>
2024-05-21 15:29:05 +0700

    add first test: test_create_profile_from_signal

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from profiles.models import Profile
+
+
+class ProfileModelTests(TestCase):
+    def test_create_profile_from_signal(self):
+        user_model = get_user_model()
+        self.assertEqual(Profile.objects.count(), 0)
+
+        new_user = user_model.objects.create(username="abc", password="def")
+
+        self.assertEqual(Profile.objects.first().user, new_user)
