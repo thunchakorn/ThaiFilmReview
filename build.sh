865bc8746e87090d1bbcc46c03b58f2a792bfc2e
thunchakorn <s.thunchakorn@gmail.com>
2024-05-27 14:49:31 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
+# load initial data
+python manage.py loaddata initial
+
 # Convert static asset files
 python manage.py collectstatic --no-input
 
