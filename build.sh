fbbdec5edb57984b06376660dabe61b3962f3767
thunchakorn <s.thunchakorn@gmail.com>
2024-05-27 14:50:55 +0700

    hot fix build.sh

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-# load initial data
-python manage.py loaddata initial
-
 # Convert static asset files
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+# load initial data
+python manage.py loaddata initial
